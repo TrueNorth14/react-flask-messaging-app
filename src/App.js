be370311c88034d4f0e43caad603b336083439d1
Vishnu Nair <truenorth514@gmail.com>
2020-08-05 23:27:24 -0400

    minor change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Login from './components/Login';
 
 const App = () => {
   return (
-    
     <Login/>
   );
 }

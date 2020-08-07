9ac4c9801516f1cbca77ddfc1e7de9e229547812
Vishnu Nair <truenorth514@gmail.com>
2020-08-07 13:09:50 -0400

    added chatroom component

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useStyles} from '@material-ui/core';
+
+
+const ChatRoom = () => {
+    return (
+        <div>
+            hello bob!
+        </div>
+    )
+}
+
+export default ChatRoom

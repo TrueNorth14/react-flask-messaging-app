ca36187ad83766992dddc0e07eb9a58a7f5493e8
Vishnu Nair <truenorth514@gmail.com>
2020-08-07 15:05:56 -0400

    added routes for login and chatroom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Login from './components/Login';
+import ChatRoom from './components/ChatRoom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <Login/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/chatroom">
+          <ChatRoom />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

219e37dfe06212ec8ca460954c4223f688e10941
TrueNorth14 <vishnu514.nair@gmail.com>
2020-08-08 22:08:17 -0400

    changed styles

--- src/assets/styles/chatroomStyle.js
+++ src/assets/styles/chatroomStyle.js
@@ -1,7 +1,12 @@
 import ChatRoom from "../../components/ChatRoom/ChatRoom"
 
 const chatRoomStyle = {
-    
+   root: {
+       maxWidth: "500px",
+       minWidth: "300px",
+    //    paddingLeft: "5rem",
+    //    paddingRight: "5rem"
+   }
 }
 
 export default chatRoomStyle;

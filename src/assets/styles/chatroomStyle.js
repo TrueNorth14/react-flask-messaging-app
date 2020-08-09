8c340201069adefe8b10197b6db0966741d5b554
TrueNorth14 <vishnu514.nair@gmail.com>
2020-08-09 12:26:37 -0400

    added root styles

--- src/assets/styles/chatroomStyle.js
+++ src/assets/styles/chatroomStyle.js
@@ -1,12 +1,15 @@
 import ChatRoom from "../../components/ChatRoom/ChatRoom"
 
 const chatRoomStyle = {
-   root: {
-       maxWidth: "500px",
-       minWidth: "300px",
-    //    paddingLeft: "5rem",
-    //    paddingRight: "5rem"
-   }
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        justifyItems: "center",
+    },
+    chatRoom: {
+        maxWidth: "500px",
+        minWidth: "300px",
+    }
 }
 
 export default chatRoomStyle;

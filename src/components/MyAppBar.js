81f5a8d8459e9ffaa8d8503fa4b366765146f2b9
TrueNorth14 <vishnu514.nair@gmail.com>
2020-08-09 12:43:10 -0400

    added MyAppBar

---
+++ src/components/MyAppBar.js
@@ -0,0 +1,19 @@
+import {
+    AppBar,
+    Toolbar,
+    Typography
+} from '@material-ui/core';
+
+const MyAppBar = () => {
+    return (
+        <AppBar >
+            <Toolbar>
+                <Typography variant="h6">
+                    Global Room
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default MyAppBar;

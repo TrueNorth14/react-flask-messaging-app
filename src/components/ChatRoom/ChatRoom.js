feb99d337e579deeb7b6161797be291d74f1c216
TrueNorth14 <vishnu514.nair@gmail.com>
2020-08-08 22:08:37 -0400

    fixed styles not loading bug

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Card, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core';
-
+import chatRoomStyle from '../../assets/styles/chatroomStyle';
 
 const ChatRoom = ({ name, ...props }) => {
+    const { classes } = props;
 
     return (
-        <div>
-            hello { name }!
-        </div>
+        <Card className={classes.root}>
+            <Typography>
+
+                hello {name} sdfsd!d
+            </Typography>
+        </Card>
     )
 }
 
-export default ChatRoom;
+export default withStyles(chatRoomStyle)(ChatRoom);

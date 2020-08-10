9d057a52effd2b392443d8830e992d0203099a1d
TrueNorth14 <vishnu514.nair@gmail.com>
2020-08-10 18:06:40 -0400

    changed log styles

--- src/assets/styles/chatroomStyle.js
+++ src/assets/styles/chatroomStyle.js
@@ -15,9 +15,23 @@ const chatRoomStyle = {
     },
     header: {
         height: "15%",
-        backgroundColor: "#545454",
+        // backgroundColor: "#545454",
         textAlign: "center"
+    },
+    input: {
+        width: "100%",
+        bottom: "0",
+        position: "absolute"
+    },
+    log: {
+        height: "70%",
+        // maxHeight: "100%",
+        // minHeight: "500px",
+        flexDirection: "column-reverse",
+        overflowY: "scroll",
+        // overflow: "auto"
     }
+
 }
 
 export default chatRoomStyle;
